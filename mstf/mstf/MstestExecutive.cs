31f27544e5c97028d1fe37685a23e2883b7e46b7
熊敏 <xiongmin@bimsop.com>
2018-01-12 18:16:36 +0800

    first commit

---
+++ mstf/mstf/MstestExecutive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mstf
+{
+    class MstestExecutive
+    {
+        private static MstestExecutive _instance = null;
+        private static readonly object locker = new object();
+        public static MstestExecutive Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (locker)
+                    {
+                        _instance = _instance ?? new MstestExecutive();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private string testAssembly = "";
+
+        private MstestExecutive()
+        {
+
+        }
+        public void Execute(string assembly)
+        {
+            testAssembly = assembly;
+
+        }
+    }
+}
---
+++ mstf/mstf/msAddin.cs
@@ -0,0 +1,21 @@
+﻿using Bentley.MstnPlatformNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mstf
+{
+    public class msAddin : AddIn
+    {
+        public msAddin(IntPtr mdlDescriptor) : base(mdlDescriptor)
+        {
+        }
+
+        protected override int Run(string[] commandLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
